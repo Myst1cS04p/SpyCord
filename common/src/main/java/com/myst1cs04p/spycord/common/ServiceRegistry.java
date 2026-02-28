813b90c592cec1c8739ed037b6c23f69f5b413d6
Myst1cS04p <myst1cs04p@gmail.com>
2026-02-28 13:07:27 +0300

    formatting

--- common/src/main/java/com/myst1cs04p/spycord/common/ServiceRegistry.java
+++ common/src/main/java/com/myst1cs04p/spycord/common/ServiceRegistry.java
@@ -10,30 +10,41 @@ import com.myst1cs04p.spycord.common.logging.FileLogSink;
 import java.util.logging.Logger;
 
 /**
-* Wires all common services together.
-* Constructed once in the platform plugin's onEnable() and passed down by reference.
-*/
+ * Wires all common services together.
+ * Constructed once in the platform plugin's onEnable() and passed down by reference.
+ */
 public class ServiceRegistry {
 
-private final IPluginConfig config;
-private final IDiscordClient discordClient;
-private final CompositeLogSink logSink;
-private final EventPipeline eventPipeline;
+    private final IPluginConfig config;
+    private final IDiscordClient discordClient;
+    private final CompositeLogSink logSink;
+    private final EventPipeline eventPipeline;
 
-public ServiceRegistry(IPluginConfig config, IDiscordClient discordClient, Logger logger) {
-this.config = config;
-this.discordClient = discordClient;
+    public ServiceRegistry(IPluginConfig config, IDiscordClient discordClient, Logger logger) {
+        this.config = config;
+        this.discordClient = discordClient;
 
-FileLogSink fileLogSink = new FileLogSink("command.log", logger);
-DiscordLogSink discordLogSink = new DiscordLogSink(discordClient);
-this.logSink = new CompositeLogSink(fileLogSink, discordLogSink);
+        FileLogSink fileLogSink = new FileLogSink("command.log", logger);
+        DiscordLogSink discordLogSink = new DiscordLogSink(discordClient);
+        this.logSink = new CompositeLogSink(fileLogSink, discordLogSink);
 
-this.eventPipeline = new EventPipeline(config, logSink);
-}
+        this.eventPipeline = new EventPipeline(config, logSink);
+    }
 
-public IPluginConfig getConfig()           { return config; }
-public IDiscordClient getDiscordClient()   { return discordClient; }
-public CompositeLogSink getLogSink()       { return logSink; }
-public EventPipeline getEventPipeline()    { return eventPipeline; }
+    public IPluginConfig getConfig() {
+        return config;
+    }
+
+    public IDiscordClient getDiscordClient() {
+        return discordClient;
+    }
+
+    public CompositeLogSink getLogSink() {
+        return logSink;
+    }
+
+    public EventPipeline getEventPipeline() {
+        return eventPipeline;
+    }
 }
 
